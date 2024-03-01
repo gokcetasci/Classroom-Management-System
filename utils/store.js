1086c07ba79387792128a4b16b48581012e178fb
gokce <gokcetascii@gmail.com>
2024-03-01 20:22:59 +0300

    store created for zustand and fake data added

---
+++ utils/store.js
@@ -0,0 +1,12 @@
+import create from 'zustand';
+
+const useStore = create((set) => ({
+  classes: [
+    { id: 1, name: 'Class 1', numericName: '101', capacity: 30 },
+    { id: 2, name: 'Class 2', numericName: '102', capacity: 25 },
+  ],
+  addClass: (formData) =>
+    set((state) => ({ classes: [...state.classes, { ...formData, id: state.classes.length + 1 }] })),
+}));
+
+export default useStore;
