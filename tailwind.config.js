e9bdd14d45e3dbe0066fff33397807395adb5fb2
gokce <gokcetascii@gmail.com>
2024-03-01 22:47:08 +0300

    colors added

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,11 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    colors: {
+      primary: "#5840bb",
+      white: "#ffffff",
+      black: "#000000",
+    }
   },
   plugins: [],
 };
