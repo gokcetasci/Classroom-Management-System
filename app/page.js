4977c8dd9d98b7990ead8cd5da520285422ba998
gokce <gokcetascii@gmail.com>
2024-03-01 23:36:11 +0300

    style added

--- app/page.js
+++ app/page.js
@@ -4,9 +4,13 @@ export default function Home() {
   return (
     <div className="container mx-auto">
       <div className="flex flex-row items-center p-12">
-        <p className="text-[26px]">Class</p>
+        <ul>
+          <li className="text-[26px]">
+            Class
+          </li>
+        </ul>
         <Link href="/class">
-          <IoIosArrowDroprightCircle className="w-9 h-9 fill-primary"/>
+          <IoIosArrowDroprightCircle className="w-9 h-9 fill-primary hover:scale-105 transition duration-300 ease-in-out"/>
         </Link>
       </div>
     </div>
