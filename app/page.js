38d321370b66dae2a4dcba36d4d4ba4f54d1f89b
gokce <gokcetascii@gmail.com>
2024-03-01 22:47:29 +0300

    icon and styles added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,14 @@
 import Link from "next/link";
-
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 export default function Home() {
   return (
-    <div>
-      <Link href="/class">
-      Class
-      </Link>
+    <div className="container mx-auto">
+      <div className="flex flex-row items-center p-12">
+        <p className="text-[26px]">Class</p>
+        <Link href="/class">
+          <IoIosArrowDroprightCircle className="w-9 h-9 fill-primary"/>
+        </Link>
+      </div>
     </div>
   );
 }
