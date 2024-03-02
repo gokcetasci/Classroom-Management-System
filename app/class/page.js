071e0b92405af92c913ae854b80f451412938b77
gokce <gokcetascii@gmail.com>
2024-03-03 02:53:47 +0300

    Added show class button status to icon

--- app/class/page.js
+++ app/class/page.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import Link from "next/link";
 import React, { useState } from "react";
 import { FaSquarePlus } from "react-icons/fa6";
@@ -15,21 +15,22 @@ function ClassPage() {
     <div>
       <div className="flex flex-row items-center p-12">
         {showClassButton && (
-          <Link href="/class">
-            <p className="text-[26px] mr-2">Class</p>
-          </Link>
+          <>
+            <Link href="/class">
+              <p className="text-[26px] mr-2">Class</p>
+            </Link>
+            <Link href="/class/addclasspage">
+              <FaSquarePlus className="w-9 h-9 fill-primary hover:scale-105 transition duration-300 ease-in-out" />
+            </Link>
+          </>
         )}
-        <Link href="/class/addclasspage">
-          <FaSquarePlus className="w-9 h-9 fill-primary hover:scale-105 transition duration-300 ease-in-out" />
-        </Link>
       </div>
-      <ClassListPage 
-      onViewStudentListClick={handleViewStudentListClick} setShowClassButton={setShowClassButton} 
+      <ClassListPage
+        onViewStudentListClick={handleViewStudentListClick}
+        setShowClassButton={setShowClassButton}
       />
-
     </div>
   );
 }
 
 export default ClassPage;
-
