0ca4708b8d9482323a0ffa3415ca09b7a9c3e963
gokce <gokcetascii@gmail.com>
2024-03-02 14:51:22 +0300

    edited

--- utils/store.js
+++ utils/store.js
@@ -22,6 +22,86 @@ const useStore = create((set) => ({
         { id: 4, name: "Student 4" },
       ],
     },
+    {
+      id: 3,
+      name: "Class 3",
+      numericName: "103",
+      capacity: 28,
+      students: [
+        { id: 5, name: "Student 5" },
+        { id: 6, name: "Student 6" },
+      ],
+    },
+    {
+      id: 4,
+      name: "Class 4",
+      numericName: "104",
+      capacity: 32,
+      students: [
+        { id: 7, name: "Student 7" },
+        { id: 8, name: "Student 8" },
+      ],
+    },
+    {
+      id: 5,
+      name: "Class 5",
+      numericName: "105",
+      capacity: 27,
+      students: [
+        { id: 9, name: "Student 9" },
+        { id: 10, name: "Student 10" },
+      ],
+    },
+    {
+      id: 6,
+      name: "Class 6",
+      numericName: "106",
+      capacity: 29,
+      students: [
+        { id: 11, name: "Student 11" },
+        { id: 12, name: "Student 12" },
+      ],
+    },
+    {
+      id: 7,
+      name: "Class 7",
+      numericName: "107",
+      capacity: 26,
+      students: [
+        { id: 13, name: "Student 13" },
+        { id: 14, name: "Student 14" },
+      ],
+    },
+    {
+      id: 8,
+      name: "Class 8",
+      numericName: "108",
+      capacity: 31,
+      students: [
+        { id: 15, name: "Student 15" },
+        { id: 16, name: "Student 16" },
+      ],
+    },
+    {
+      id: 9,
+      name: "Class 9",
+      numericName: "109",
+      capacity: 24,
+      students: [
+        { id: 17, name: "Student 17" },
+        { id: 18, name: "Student 18" },
+      ],
+    },
+    {
+      id: 10,
+      name: "Class 10",
+      numericName: "110",
+      capacity: 30,
+      students: [
+        { id: 19, name: "Student 19" },
+        { id: 20, name: "Student 20" },
+      ],
+    },
   ],
   addClass: (formData) => {
     set((state) => {
