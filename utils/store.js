6cae03082287bc64bab2057bcb3ad316d226fd3d
gokce <gokcetascii@gmail.com>
2024-03-03 01:14:13 +0300

    student email datas added

--- utils/store.js
+++ utils/store.js
@@ -8,8 +8,8 @@ const useStore = create((set) => ({
       numericName: "101",
       capacity: 30,
       students: [
-        { id: 1, name: "Student 1" },
-        { id: 2, name: "Student 2" },
+        { id: 1, name: "Student 1", email: "student1@example.com" },
+        { id: 2, name: "Student 2", email: "student2@example.com" },
       ],
     },
     {
@@ -18,8 +18,8 @@ const useStore = create((set) => ({
       numericName: "102",
       capacity: 25,
       students: [
-        { id: 3, name: "Student 3" },
-        { id: 4, name: "Student 4" },
+        { id: 3, name: "Student 3", email: "student3@example.com" },
+        { id: 4, name: "Student 4", email: "student4@example.com" },
       ],
     },
     {
@@ -28,8 +28,8 @@ const useStore = create((set) => ({
       numericName: "103",
       capacity: 28,
       students: [
-        { id: 5, name: "Student 5" },
-        { id: 6, name: "Student 6" },
+        { id: 5, name: "Student 5", email: "student5@example.com" },
+        { id: 6, name: "Student 6", email: "student6@example.com" },
       ],
     },
     {
@@ -38,8 +38,8 @@ const useStore = create((set) => ({
       numericName: "104",
       capacity: 32,
       students: [
-        { id: 7, name: "Student 7" },
-        { id: 8, name: "Student 8" },
+        { id: 7, name: "Student 7", email: "student7@example.com" },
+        { id: 8, name: "Student 8", email: "student8@example.com" },
       ],
     },
     {
@@ -48,8 +48,8 @@ const useStore = create((set) => ({
       numericName: "105",
       capacity: 27,
       students: [
-        { id: 9, name: "Student 9" },
-        { id: 10, name: "Student 10" },
+        { id: 9, name: "Student 9", email: "student9@example.com" },
+        { id: 10, name: "Student 10", email: "student10@example.com" },
       ],
     },
     {
@@ -58,8 +58,8 @@ const useStore = create((set) => ({
       numericName: "106",
       capacity: 29,
       students: [
-        { id: 11, name: "Student 11" },
-        { id: 12, name: "Student 12" },
+        { id: 11, name: "Student 11", email: "student11@example.com" },
+        { id: 12, name: "Student 12", email: "student12@example.com" },
       ],
     },
     {
@@ -68,8 +68,8 @@ const useStore = create((set) => ({
       numericName: "107",
       capacity: 26,
       students: [
-        { id: 13, name: "Student 13" },
-        { id: 14, name: "Student 14" },
+        { id: 13, name: "Student 13", email: "student13@example.com" },
+        { id: 14, name: "Student 14", email: "student14@example.com" },
       ],
     },
     {
@@ -78,8 +78,8 @@ const useStore = create((set) => ({
       numericName: "108",
       capacity: 31,
       students: [
-        { id: 15, name: "Student 15" },
-        { id: 16, name: "Student 16" },
+        { id: 15, name: "Student 15", email: "student15@example.com" },
+        { id: 16, name: "Student 16", email: "student16@example.com" },
       ],
     },
     {
@@ -88,8 +88,8 @@ const useStore = create((set) => ({
       numericName: "109",
       capacity: 24,
       students: [
-        { id: 17, name: "Student 17" },
-        { id: 18, name: "Student 18" },
+        { id: 17, name: "Student 17", email: "student17@example.com" },
+        { id: 18, name: "Student 18", email: "student18@example.com" },
       ],
     },
     {
@@ -98,8 +98,8 @@ const useStore = create((set) => ({
       numericName: "110",
       capacity: 30,
       students: [
-        { id: 19, name: "Student 19" },
-        { id: 20, name: "Student 20" },
+        { id: 19, name: "Student 19", email: "student19@example.com" },
+        { id: 20, name: "Student 20", email: "student20@example.com" },
       ],
     },
   ],
@@ -127,23 +127,29 @@ const useStore = create((set) => ({
     set((state) => ({
       classes: state.classes.filter((classInfo) => classInfo.id !== classId),
     })),
-    addStudent: (classId, studentName) =>
-    set((state) => {
-      console.log('Updating state with new student:', classId, studentName);
-      const updatedClasses = state.classes.map((classInfo) =>
-        classInfo.id === classId
-          ? {
-              ...classInfo,
-              students: [
-                ...classInfo.students,
-                { id: classInfo.students.length + 1, name: studentName },
-              ],
-            }
-          : classInfo
-      );
-  
-      return { classes: updatedClasses };
-    }),
+    
+    addStudent: (classId, studentName, studentEmail) =>
+  set((state) => {
+    console.log('Updating state with new student:', classId, studentName, studentEmail);
+    const updatedClasses = state.classes.map((classInfo) =>
+      classInfo.id === classId
+        ? {
+            ...classInfo,
+            students: [
+              ...classInfo.students,
+              { 
+                id: classInfo.students.length + 1,
+                name: studentName,
+                email: studentEmail,
+              },
+            ],
+          }
+        : classInfo
+    );
+
+    return { classes: updatedClasses };
+  }),
+
   
   
   
