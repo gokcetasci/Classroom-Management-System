ff683d40b6e0fd8a854ce31466b072e7978229f5
gokce <gokcetascii@gmail.com>
2024-03-02 13:52:20 +0300

    edited

--- app/class/page.js
+++ app/class/page.js
@@ -1,21 +1,21 @@
-import ClassListPage from '@/components/classlistpage'
-import Link from 'next/link'
-import React from 'react'
+import ClassListPage from "@/components/classlistpage";
+import Link from "next/link";
+import React from "react";
 import { FaSquarePlus } from "react-icons/fa6";
 function ClassPage() {
   return (
     <div>
-    <div className="flex flex-row items-center p-12">
-    <p className="text-[26px] mr-2">
-      Class  
-    </p>
-    <Link href="/class/addclasspage">
-    <FaSquarePlus className="w-9 h-9 fill-primary hover:scale-105 transition duration-300 ease-in-out"/>
-    </Link>
+      <div className="flex flex-row items-center p-12">
+        <Link href="/class">
+          <p className="text-[26px] mr-2">Class</p>
+        </Link>
+        <Link href="/class/addclasspage">
+          <FaSquarePlus className="w-9 h-9 fill-primary hover:scale-105 transition duration-300 ease-in-out" />
+        </Link>
+      </div>
+      <ClassListPage />
     </div>
-    <ClassListPage/>
-    </div>
-  )
+  );
 }
 
-export default ClassPage
+export default ClassPage;
