fa0e5fe05e4977e0ca6018d50dd50694f9027c02
gokce <gokcetascii@gmail.com>
2024-05-01 12:53:55 +0300

    edited

--- app/page.js
+++ app/page.js
@@ -1,18 +1,10 @@
 import Link from "next/link";
 import { IoIosArrowDroprightCircle } from "react-icons/io";
+import ClassPage from "./class/page";
 export default function Home() {
   return (
     <div className="container mx-auto">
-      <div className="flex flex-row items-center p-12">
-        <ul>
-          <li className="text-[26px] mr-2">
-            Sınıf
-          </li>
-        </ul>
-        <Link href="/class">
-          <IoIosArrowDroprightCircle className="w-9 h-9 fill-primary hover:scale-105 transition duration-300 ease-in-out"/>
-        </Link>
-      </div>
+      <ClassPage/>
     </div>
   );
 }
